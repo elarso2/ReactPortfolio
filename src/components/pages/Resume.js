f5dce2ef6d2b173ef3014edcd89fd564f80e275b
Elizabeth Larson <elarso2@uw.edu>
2022-07-20 16:12:48 -0700

    adding resume skill lists

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -13,26 +13,30 @@ export default function Resume() {
       </h2>
       <h3>Front-End Proficiencies</h3>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>JQuery</li>
+        <li>React</li>
+        <li>Bootstrap</li>
       </ul>
       <h3>Back-End Proficiencies</h3>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>MySQL, Sequelize</li>
+        <li>API's</li>
+        <li>MongoDB</li>
+        <li>Node.js</li>
+        <li>Handlebars.js</li>
+        <li>Express.js</li>
       </ul>
       <h3>Skills</h3>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>Slack</li>
+        <li>Microsoft Office</li>
+        <li>Google Drive</li>
+        <li>Conversational Spanish Fluency</li>
+        <li>Project Management</li>
+        <li>Communication and Conflict Resolution</li>
       </ul>
     </div>
   );
