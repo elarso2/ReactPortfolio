b9e517c1550b7328452e7bb3ccc68a545a5c936d
Elizabeth Larson <elarso2@uw.edu>
2022-07-20 23:47:17 -0700

    adding deployment links to work page

--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -14,7 +14,9 @@ export default function myWork() {
       <div className="projects">
         <section>
           <img className="workImg" alt="" src="../../assets/E-Commerce.PNG" />
-          <h3>E-Commerce Back End Application</h3>
+          <h3 href="https://drive.google.com/file/d/1-ZGft5yOfgP3JPVx74e77rpijLf5KH1x/view">
+            E-Commerce Back End Application
+          </h3>
           <a href="https://github.com/elarso2/E-CommerceBackEnd">
             <i class="fa-brands fa-github"></i>
           </a>
@@ -24,13 +26,15 @@ export default function myWork() {
           alt=""
           src="../../assets/barbellsAndBrews.png"
         >
-          <h3>Barbells and Brews</h3>
+          <h3 href="https://simonasnap.github.io/WorkoutMusicAPI/">
+            Barbells and Brews
+          </h3>
           <a href="https://github.com/SimonaSnap/WorkoutMusicAPI">
             <i class="fa-brands fa-github"></i>
           </a>
         </section>
         <section className="workImg" alt="" src="../../assets/noteTaker.png">
-          <h3>Note Taker</h3>
+          <h3 href="https://note-taker-elarson.herokuapp.com/">Note Taker</h3>
           <a href="https://github.com/elarso2/NoteTaker">
             <i class="fa-brands fa-github"></i>
           </a>
@@ -40,13 +44,15 @@ export default function myWork() {
           alt=""
           src="../../assets/passwordGenerator.png"
         >
-          <h3>Password Generator</h3>
+          <h3 href="https://elarso2.github.io/Password-Generator/">
+            Password Generator
+          </h3>
           <a href="https://github.com/elarso2/Password-Generator">
             <i class="fa-brands fa-github"></i>
           </a>
         </section>
         <section className="workImg" alt="" src="../../assets/textEditor.png">
-          <h3>Text Editor</h3>
+          <h3 href="https://ell-text-editor.herokuapp.com/">Text Editor</h3>
           <a href="https://github.com/elarso2/Text-Editor">
             <i class="fa-brands fa-github"></i>
           </a>
